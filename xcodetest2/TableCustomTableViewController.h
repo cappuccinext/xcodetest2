78aaddd57eb94d30ea142fc7b335975160f29d7f
cappuccinext <cappuccinext@Haswell.local>
2014-07-12 14:37:29 +0900

    new created project
    a new project created from spot list

---
+++ xcodetest2/TableCustomTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableCustomTableViewController.h
+//  xcodetest2
+//
+//  Created by cappuccinext on 2014/07/11.
+//  Copyright (c) 2014年 cappmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableCustomTableViewController : UITableViewController
+
+@end
